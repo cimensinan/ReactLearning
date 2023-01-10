939a9cec268aa0ebd2f92504ee4bba156d9de50b
Sinan Çimen <cimensinan89@gmail.com>
2023-01-10 21:08:50 +0300

    comments

--- src/components/09-image/Gallery.js
+++ src/components/09-image/Gallery.js
@@ -2,13 +2,17 @@ import React from 'react'
 import "./gallery.scss"
 import images from "./images.json"
 
+// json'dan ya da backend'den dosya yolu gelmemeli sadece dosya ismi gelmesi gerekir. Çünkü içindeki dosyayı benim nereye yerleştireceğimi her zaman bilmeleri mümkün değildir.
+
 const Gallery = () => {
   return (
     <div className='gallery'>
         {
             images.map((img, i) => 
             <div key={i}>
+              {/* require ile birlikte src'nin içinde resimleri nereye koyduksak oranın dosya yolunu yazmak gerekiyor. */}
                 <a href={require(`../../assets/img/${img.name}`)}target="_blank"><img src={require(`../../assets/img/${img.name}`)} alt="" /></a>
+                {/* Backtick'li kullanımlarda string bir ifadeye bir yerden bir veri çağırıyorsak ${} kullanmak gerekiyor */}
             </div>
             )
         }
--- src/components/09-image/image.js
+++ src/components/09-image/image.js
@@ -4,18 +4,21 @@ import northernLights from "../../assets/img/image1.jpg"
 const Image = () => {
   return (
     <div>
-        {/* Statik yöntemde görüntüler public klasörü içinde aranır */}
+        {/* HTML ce CSS'i hazır gelen projelerde img dosya yolu ile uğraşmamak için img'ler publicte oluşturulan aseets dosyasının içine atılır ve statik yöntemle oradan çekilir. */}
+        {/* Statik yöntemde görüntüler public klasörü içinde aranır, src'de aranmaz. */}
         <img src="assets/img/car.jpg" alt="" width="300"/>
 
         {/* External URL veya CDN yöntemi*/}
+        {/* Doğrudan resmin kopyalanan adresi src kısmına yapıştırılır. */}
         <img src="https://images.unsplash.com/photo-1502082553048-f009c37129b9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fG5hdHVyZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" width="300"/>
 
         {/* Import ve require yönteminde dosyalar src içinde olmalıdır. */}
+        {/* src klasöründeki görüntülerin eklenmesi için kullanılırlar. Eğer görüntünün adı dinamik olarak bir dış kaynaktan alınıyorsa bu durumda require kullanmak gerekir. */}
         <img src={northernLights} alt="" width="300"/>
 
         <img src={require("../../assets/img/image2.jpg")} alt="" width="300"/>
     </div>
   )
-}
+} 
 
 export default Image
