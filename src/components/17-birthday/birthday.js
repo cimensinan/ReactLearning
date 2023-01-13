25f5d5e83c186187594662df13835a4f2fdc8d63
Sinan Çimen <cimensinan89@gmail.com>
2023-01-13 21:04:24 +0300

    comments

--- src/components/17-birthday/birthday.js
+++ src/components/17-birthday/birthday.js
@@ -5,10 +5,11 @@ import data from "./people.json"
 
 const Birthday = () => {
 
-    const [people, setPeople] = useState(data);
+    const [people, setPeople] = useState(data); // ilk değerini json dosyamızdan alıyoruz. useState kullanmadan önce biz json dosyalarını manipüle edemiyorduk. Bu şekilde edebileceğiz.
 
     const deletePerson = (id) => {
       const arr = people.filter((item) => item.id !== id)
+      // ilgili id'ye sahip olmayan elemanları filtrele demiş olduk
       setPeople(arr);
     }
 
@@ -17,14 +18,22 @@ const Birthday = () => {
         <h1>Bugün Doğanlar</h1>
         <p>Bugün doğan {people.length} kişi bulundu</p>
         {
+          // Artık map'leme işlerimizi json'dan değil json'ın tutulduğu getter değerlerimizden yapacağız.
             people.map((item) => (
                 <CardPerson key={item.id} {...item} deletePerson={deletePerson}/>
                 // <CardPerson key={item.id} {...item} setPeople={setPeople}/>
+                // prop göndermenin kısa yolu spread operatörü kullanmaktır.
+                // CardPerson componentimize buradan deletePerson'u prop olarak gönderdik. Süslü parantez içine yazdığım ise fonksiyonum. Genelde tanımlayıcı ile fonksiyonun isimleri aynı kullanılıyor, karışıklık böylece engellenebiliyor.
             ))
         }
         <Button className='me-1' variant='danger' onClick={() => setPeople([])}>Remove All</Button>
+        {/* Boş dizi getir demiş olduk */}
+        {/* setPeople(null) yapmış olsaydık bu değeri gönderdiğimizde people'ın içi null değerine sahip olacak ve people kullandığımız diğer satırlarda hata alacağımız için ekranda herhangi bişey gözükmeyecektir. */}
         <Button className='me-1' variant='success' onClick={() => setPeople(data)}>Reload Data</Button>
+        {/* data dizisini tekrar getir dedik. */}
         <Button className='me-1' variant='primary' onClick={() => setPeople([data[0]])}>Reload One Data</Button>
+        {/* Burada setPeople(data[0]) şeklinde kullanımımızda dizi getirmez data'nın içindeki ilgili indexdeki objeyi getirir. Bu nedenle tekrardan dizi içerisine almamız gerekiyor. */}
+        {/* Burada yer verdiğimiz variant, onClick gibi ifadelerin hepsi aslında bootstrap'in propları key olarak bu ifadeyi kullanıp karşısına da value'sini yazıyoruz. */}
     </Container>
   )
 }
--- src/components/17-birthday/cardPerson.js
+++ src/components/17-birthday/cardPerson.js
@@ -3,6 +3,7 @@ import { Card, Col, Row } from 'react-bootstrap'
 import { FaTrash } from "react-icons/fa"
 
 const CardPerson = ({image, name, age, id, deletePerson}) => {
+    // deletePerson'u da burada prop değeri olarak ekliyoruz.
   return (
     <Card className='mb-4 position-relative'>
         <Row>
@@ -12,11 +13,13 @@ const CardPerson = ({image, name, age, id, deletePerson}) => {
             <Col md={10}>
                 <Card.Body>
                     <Card.Title className='fs-1'>{name}</Card.Title>
+                    {/* Bootstrap'in h1 adında bir class'ı var ancak burada Bootstrap'in card yapısını kullanırken otomatik olarak gelen bir h5 class'ı var. Bunu h1 class'ı vererek ezmek mümkün değil çünkü h5 class bootstrap'te hedefe daha yakın. Bu nedenle farklı bir class kullanım yoluna gittik font'u büyüttük. */}
                     <Card.Subtitle>{age}</Card.Subtitle>
                 </Card.Body>
             </Col>
         </Row>
         <span className='position-absolute end-0 bottom-0 me-2 mb-2 text-danger' onClick={() => deletePerson(id)}>
+            {/* deletePerson burada silinecek bir id değeri beklediği için parametre olarak id ifadesini ekliyoruz. */}
             <FaTrash/>
         </span>
     </Card>
