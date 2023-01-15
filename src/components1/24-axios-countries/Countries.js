36e60a84438c06f1f830a62f1c570c61fc4697e8
Sinan Çimen <cimensinan89@gmail.com>
2023-01-15 23:30:15 +0300

    axios-comments

--- src/components1/24-axios-countries/Countries.js
+++ src/components1/24-axios-countries/Countries.js
@@ -3,16 +3,21 @@ import React, { useEffect, useState } from 'react'
 import { Table } from 'react-bootstrap'
 
 const Countries = () => {
-
+    // Bize axios ile gelen data json formatında geldiği için ve bu json bir dizi olduğu için başlangıç değerimizi boş dizi yapıyoruz.
     const [countries, setCountries] = useState([])
+    // Sayfa ya da veriler yüklenene kadar geçen süre zarfında bir snippet çalışsın istiyorsak ona ilişkinde ayrı bir state tanımlaması yapmak gerekiyor. Başlangıçta gözükmesini istediğimiz için true değerini veriyoruz.
     const [loading, setLoading] = useState(true)
 
+    // await'li yani bekletmeli bir yapı kullandığımızda bunu fonksiyonumuzda ifade etmek gerekiyor ve "async" diyerek asenkron bir yapı kullanacağımız haber veriyoruz.
     const loadData = async () => {
+        // axios'u await'li yaptığımız için ve then kullanmamak için bu şekilde bir değişken daha tanımlıyoruz.
         const resp = await axios.get("https://restcountries.com/v3.1/all")
+        // Bu haliyle bize response yapı gelir. Bize bu response'dan sadece data gerektiği için onu çağırıyoruz ve setCountries ile bu datayı countries'in içine alıyoruz.
         console.log(resp.data)
         setCountries(resp.data)
     }
 
+    // useEffect'in içini çok kalabalıklaştırmamak için içeri tarafa yazacaklarımızı ayrı bir fonksiyon yaparız.
     useEffect(() => {
       loadData()
     }, [])
